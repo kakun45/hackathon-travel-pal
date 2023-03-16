d24e86f0bf6c60dee4905e4533515f80c67a3dbd
kakun45 <email4blackkat@gmail.com>
2023-03-16 18:21:14 -0400

    set up Results Page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 // import logo from './logo.svg';
-import './App.scss';
-import Main from './components/Main';
-import Header from './components/Header/Header';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Itinerary from './pages/Itinerary';
-import { useState } from 'react';
+import "./App.scss";
+import Main from "./components/Main";
+import Header from "./components/Header/Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Itinerary from "./pages/Itinerary";
+import { useState } from "react";
+import Results from "./pages/resultsPage";
 
 function App() {
-  const [itineraryList, setItineraryList] = useState([])
+  const [itineraryList, setItineraryList] = useState([]);
 
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path='/' element={<Main setItineraryList={setItineraryList} />} />
-        <Route path='/itinerary'
-          element={<Itinerary setItineraryList={setItineraryList} itineraryList={itineraryList} />} />
-
+        <Route
+          path="/"
+          element={<Main setItineraryList={setItineraryList} />}
+        />
+        <Route
+          path="/itinerary"
+          element={
+            <Itinerary
+              setItineraryList={setItineraryList}
+              itineraryList={itineraryList}
+            />
+          }
+        />
+        <Route path="/results" element={<Results />} />
       </Routes>
     </BrowserRouter>
   );
-};
+}
 
 export default App;

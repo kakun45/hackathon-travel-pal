58f398d5bb71866ca76bc7a22da34f7cee8ee0cd
kakun45 <email4blackkat@gmail.com>
2023-03-16 19:02:51 -0400

    modified Results Page

--- src/pages/resultsPage.js
+++ src/pages/resultsPage.js
@@ -1,13 +1,13 @@
-import Card from "./";
+import ItineraryCard from "../components/ItineraryCard/ItineraryCard";
 
-const Results = ({ CHANGEmeCardData }) => {
+const Results = ({ image, event, time, pplNum }) => {
   return (
     <>
       <div className="CHANGE-ME-card">
         <ul className="CHANGE-ME-listParent">
           <li>
-            <img className="CHANGE-ME-img" src={CHANGEmeCardData.image}>
-              <iternaryCard />
+            <img className="CHANGE-ME-img" src={image}>
+              <ItineraryCard />
             </img>
           </li>
         </ul>

07eb853b3538a7334a9a404d2727a4f838519a8f
kakun45 <email4blackkat@gmail.com>
2023-03-16 18:42:37 -0400

    modified Results Page

--- src/pages/resultsPage.js
+++ src/pages/resultsPage.js
@@ -1,7 +1,17 @@
-const Results = () => {
+import Card from "./";
+
+const Results = ({ CHANGEmeCardData }) => {
   return (
     <>
-      <div></div>{" "}
+      <div className="CHANGE-ME-card">
+        <ul className="CHANGE-ME-listParent">
+          <li>
+            <img className="CHANGE-ME-img" src={CHANGEmeCardData.image}>
+              <iternaryCard />
+            </img>
+          </li>
+        </ul>
+      </div>{" "}
     </>
   );
 };

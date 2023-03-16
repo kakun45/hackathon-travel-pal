ef6081aafc717a4aa3d5edc5399e648dc2a73d58
Lin Lin <lin2013lin@gmail.com>
2023-03-16 19:01:41 -0400

    refactor itinerary list map

--- src/components/ItineraryCard/ItineraryCard.js
+++ src/components/ItineraryCard/ItineraryCard.js
@@ -1,6 +1,14 @@
 import './ItineraryCard.scss'
 
-const ItineraryCard = ({ event, time, pplNum }) => {
+const ItineraryCard = ({ event, time, pplNum, id, image, setItineraryList }) => {
+
+    // function removeEvent(eventId) {
+    //     setItineraryList(preval => preval.filter(preval.id !== eventId))
+    // }
+
+    // onClick = {() => removeEvent(id)}
+
+
     return (
         <></>
     )

cb48150c468a0ca80eb1237e7f80ed2cc8ed3d8b
Alexa <alexaa6415@gmail.com>
2023-03-17 15:05:00 -0400

    mercged changes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import "./Header.scss";
 import { Link } from 'react-router-dom';
-import Nav from "../../assets/images/nav4.png"; import Nav from "../../assets/images/nav4.png";
+import Nav from "../../assets/images/nav4.png";
 
 
 const Header = () => {
@@ -9,7 +9,7 @@ const Header = () => {
             <Link to='/'>
                 <div className="header__div">
                     <h1 className="header__title">TRAVEL PAL</h1>
-                    <img src={Nav} alt="nav" className="header__nav"></img>
+                    {/* <img src={Nav} alt="nav" className="header__nav"></img> */}
                 </div>
             </Link>
         </header >
--- src/pages/final.js
+++ src/pages/final.js
@@ -2,18 +2,9 @@ import './final.scss';
 import ItineraryCard from '../components/ItineraryCard/ItineraryCard';
 
 function Final() {
-<<<<<<< HEAD
     return (<></>)
     // Remove Buttons
     // Click to join button
-
-=======
-
-    return (<>
-
-
-    </>)
->>>>>>> feature/alexa
 }
 
 export default Final;

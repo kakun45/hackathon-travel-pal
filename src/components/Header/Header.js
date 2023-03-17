d66a6b60a519db4e301fdc160319c975c30cf7b5
Lin Lin <lin2013lin@gmail.com>
2023-03-16 23:20:29 -0400

    commented out nav

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,13 +8,13 @@ const Header = () => {
                 <div className="header__div">
                     <h1 className="header__title">TRAVEL PAL</h1>
                 </div>
-                <div class="navigation__links--section">
+                {/* <div class="navigation__links--section">
                     <ul class="navigation__tool">
                         <li class="navigation__link">HOME</li>
                         <li class="navigation__link">Contact Us</li>
                         <li class="navigation__link" >About</li>
                     </ul>
-                </div>
+                </div> */}
             </nav>
             <section className="main">
                 <div className="main__div">
--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,7 @@ import { useState } from "react"
 import axios from "axios"
 import { useNavigate } from 'react-router-dom';
 import Button from "./Button/Button";
-import "./Main.scss"; import Button from "./Button/Button"
+import "./Main.scss";
 
 
 const Main = ({ setItineraryList }) => {

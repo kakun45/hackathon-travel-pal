735a500e8d27eea9d967df7e11472a139a73726f
Lin Lin <lin2013lin@gmail.com>
2023-03-16 23:42:23 -0400

    header glitch

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,19 +8,7 @@ const Header = () => {
                 <div className="header__div">
                     <h1 className="header__title">TRAVEL PAL</h1>
                 </div>
-                {/* <div class="navigation__links--section">
-                    <ul class="navigation__tool">
-                        <li class="navigation__link">HOME</li>
-                        <li class="navigation__link">Contact Us</li>
-                        <li class="navigation__link" >About</li>
-                    </ul>
-                </div> */}
             </nav>
-            {/* <section className="main">
-                <div className="main__div">
-                    <h2 className="main__title">WELCOME TO VEGAS</h2>
-                </div>
-            </section> */}
         </header >
     )
 };

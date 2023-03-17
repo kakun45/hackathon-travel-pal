4e118b183903a942db0b6f2901492fb11331763e
Alexa <alexaa6415@gmail.com>
2023-03-17 15:08:04 -0400

    Finalized styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,14 +2,13 @@ import "./Header.scss";
 import { Link } from 'react-router-dom';
 import Nav from "../../assets/images/nav4.png";
 
-
 const Header = () => {
     return (
         <header>
             <Link to='/'>
                 <div className="header__div">
                     <h1 className="header__title">TRAVEL PAL</h1>
-                    {/* <img src={Nav} alt="nav" className="header__nav"></img> */}
+                    <img src={Nav} alt="nav" className="header__nav"></img>
                 </div>
             </Link>
         </header >
